54996d1fe2c6b994c2c0ae4d7cdbb255bcb94a32
rajivranjanmars <rajivranjanmars@github.com>
2021-03-26 15:00:18 +0530

    🔵

---
+++ strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+  string a,b;
+  cin >>a >>b;
+  cout << a.length() <<" " <<b.length() <<endl;
+  cout << a+b << endl;
+  char temp;
+   temp= a[0];
+   a[0]=b[0];
+   b[0]=temp;
+  cout << a<<" " <<b <<endl;
+    return 0;
+}
+

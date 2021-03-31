ad6de263b33a073ca635e0b9ec0d54bd4596dfd3
rajivranjanmars <rajivranjanmars@gmail.com>
2021-03-31 14:45:34 +0530

    https://www.hackerrank.com/challenges/cpp-input-and-output/problem

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b,c;
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+
+    cin >> a >> b >>c;
+    cout << a+b+c;  
+    return 0;
+}

31b7472d4e132bdca3c7946ac00e812e55742bc6
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-06 15:33:34 +0530

    https://www.hackerrank.com/challenges/arrays-introduction/problem

---
+++ arrays.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = n - 1; i >= 0; i--)
+        cout << arr[i] << " ";
+
+    return 0;
+}

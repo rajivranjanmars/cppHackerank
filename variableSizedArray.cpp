fe740e94cb34f2ca7c0ade37e2d1568ee4626ed2
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-07 21:01:36 +0530

    .

--- variableSizedArray.cpp
+++ variableSizedArray.cpp
@@ -1,68 +1,42 @@
-#include <algorithm>
 #include <cmath>
 #include <cstdio>
-#include <iostream>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-    int n, q;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n,q;
     cin >> n >> q;
-    vector<vector<int>> v1;
+    vector<vector<int>>v1;
     vector<int> v2;
-    for (int i = 0; i < n; i++){
-        int k ;
+    for(int i = 0; i <n; i++){
+        int k;
         cin >> k;
-        for (int j = 0; j <k; j++){
-             int x;
+        for(int j = 0; j <k; j++){
+            int x;
             cin >> x;
             v2.push_back(x);
         }
         v1.push_back(v2);
         v2.clear();
     }
-   vector<vector<int>> q1;
+    vector<vector<int>>q1;
     vector<int> q2;
-    for (int i = 0; i < q; i++){
-       
-        for (int j = 0; j <2; j++){
-             int x;
+    for(int i = 0; i <q; i++){
+        for(int j = 0; j <2;j++){
+            int x;
             cin >> x;
             q2.push_back(x);
         }
-        q1.push_back(v2);
+        q1.push_back(q2);
         q2.clear();
-    }    
-    for (int i = 0; i < q; i++)
-    {
-       cout << q[i][2] << endl;;   
-      
-    } 
-}
-
-  
-/*   int n, q;
-    cin >> n >> q;
-    int varArr[n][];
-    for (int i = 0; i < n; i++)
-    {
-        int k;
-        for (int j = 0; j < k; j++)
-            cin >> varArr[i][j];
-    }
-    int query[q][2];
-    for (int i = 0; i < q; i++)
-    {
-        for (int j = 0; j < 2; j++)
-            cin >> query[i][j];
-    }
-    
-    for (int i = 0; i < q; i++)
-    {
-        for (int j = 0; j < 2; j++)
-            cout << varArr[query[i][j]][query[i][j]]<< endl;
     }
 
+    for(int i = 0;i<q;i++){
+        cout << q1[i][2] <<endl;
+    }
     return 0;
-}*/
+}

5540e2577718326f204f94162d18e1015ac5f01f
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-10 18:50:17 +0530

    https://www.hackerrank.com/challenges/vector-sort/problem

---
+++ vectorSort.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}

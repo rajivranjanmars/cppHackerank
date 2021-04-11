b3b09c7b559db8797ba8ebc873d6f79113edc30a
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-11 14:21:08 +0530

    https://www.hackerrank.com/challenges/c-tutorial-strings/problem

--- strings.cpp
+++ strings.cpp
@@ -2,17 +2,17 @@
 #include <string>
 using namespace std;
 
-int main() {
-	// Complete the program
-  string a,b;
-  cin >>a >>b;
-  cout << a.length() <<" " <<b.length() <<endl;
-  cout << a+b << endl;
+int main()
+{
+  // Complete the program
+  string a, b;
+  cin >> a >> b;
+  cout << a.length() << " " << b.length() << endl;
+  cout << a + b << endl;
   char temp;
-   temp= a[0];
-   a[0]=b[0];
-   b[0]=temp;
-  cout << a<<" " <<b <<endl;
-    return 0;
+  temp = a[0];
+  a[0] = b[0];
+  b[0] = temp;
+  cout << a << " " << b << endl;
+  return 0;
 }
-
